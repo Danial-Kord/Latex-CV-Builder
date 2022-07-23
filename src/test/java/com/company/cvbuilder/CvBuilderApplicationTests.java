c613bef86910d4bf63a989743091669936638c53
Danial <danial.kordmodanlou@gmail.com>
2022-07-23 18:55:02 +0430

    [feat]: Spring Boot application initiated successfully

---
+++ src/test/java/com/company/cvbuilder/CvBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.cvbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
